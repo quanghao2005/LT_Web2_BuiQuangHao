846aaa31b855f06dd9a51e4c811e233783ed04c4
Bùi Quang Hào <hao01679462492@gmail.com>
2026-06-05 08:31:16 +0700

    first commit

---
+++ src/main/java/com/example/BuiQuangHao/BuiQuangHaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.BuiQuangHao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuiQuangHaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuiQuangHaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BuiQuangHao/BuiQuangHaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BuiQuangHao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuiQuangHaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
